460ccc5588b40c92dd2f597df88389f1a09ad604
root <root@puppet.example.com>
2015-09-01 07:05:09 -0400

    Dispatcher+apache2+

--- dispatcher/manifests/files.pp
+++ dispatcher/manifests/files.pp
@@ -8,9 +8,23 @@ file { '/tmp/dispatcher.any':
         group => 'root',
         mode => '0755',
 }
+file { '/tmp/vhosts.conf':
+        content => template('dispatcher/vhosts.erb'),
+        ensure => present,
+        owner => 'root',
+        group => 'root',
+        mode => '0755',
+}
+file { '/usr/lib/apache2/modules/dispatcher-apache2.so':
+        content => template('dispatcher/dispatcher-apache2.4-4.1.10.so.erb'),
+        ensure => present,
+        owner => 'root',
+        group => 'root',
+        mode => '0755',
+}
 exec { 'mv':
 	path => ['/usr/bin', '/usr/sbin', '/bin'],
-	command => "mkdir -p ${dir} && mv /tmp/dispatcher.any ${dir}",
+	command => "mkdir -p ${dir} && mv /tmp/dispatcher.any ${dir} && mv /tmp/vhosts.conf ${dir}",
 	require => File['/tmp/dispatcher.any'],
 }
 #exec { 'dispatcher_dir':
